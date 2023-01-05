91f3f5f8be7b65a8b64a85f2787d15526628ad4d
Marcellocosti <marcellodicostanzo00@gmail.com>
2023-01-05 10:13:31 +0100

    Vertex Reconstruction with Tracklet Dca

--- Simulation/Reconstruction.cpp
+++ Simulation/Reconstruction.cpp
@@ -1,6 +1,7 @@
 #include "Reconstruction.h"
 #include"TDirectory.h"
 #include "TH1D.h"
+#include "TAxis.h"
 #include "TCanvas.h"
  
 ClassImp(Reconstruction)
@@ -60,16 +61,21 @@ Reconstruction::Reconstruction()
     
     TrackletsReco();
     cout << "TrackletsReco ended" << endl;
-    MinDca();
-    cout << "MinDca ended" << endl;
-    FillHistoResiduals();
-    cout << "FillHIstoResiduals ended" << endl;
+    //MinDca();
+    //cout << "MinDca ended" << endl;
+    //FillHistoResiduals();
+    //cout << "FillHIstoResiduals ended" << endl;
+    MinGlobalDistance();
+    cout << "MinGlobalDistance ended" << endl;
     file->Close();
-    TFile outfile("outfile.root","recreate");
-    fResiduals->Write();
-    outfile.Close();
+    //TFile outfile("outfile.root","recreate");
+    //fResiduals->Write();
+    //outfile.Close();
+    /*TFile outfile("trackletdca.root","recreate");
+    ftrackletdca->Write();
+    trackletdca.Close();*/
 }
- 
+
 void Reconstruction::TrackletsReco()
 {
     //cout << "Entering FindTracklets" << endl;
@@ -77,76 +83,31 @@ void Reconstruction::TrackletsReco()
     std::vector<MaterialBudget::fPoint> tracklet;
     for (unsigned i=0; i<fIntersections1.size(); ++i)
     {
-        for (auto y: fIntersections1[i])
-            for (auto j: fIntersections2[i])
+        int count=0;
+        for (auto y: fIntersections1[i]){
+            for (auto j: fIntersections2[i]){
                 if(abs(y.phi-j.phi)<phimax)
                 {
                     tracklet.push_back(y);
                     tracklet.push_back(j);
+                    count++;
                 }
+            }
+        }
+        cout << "Reconstructed tracklets: " << count << endl;
         if (tracklet.size()>0)
             fTracklets.push_back(tracklet);
         tracklet.clear();
     }
+    //for(unsigned i=0; i<fTracklets.size(); i++){
+    //    cout << "Event " << i << " tracklets: " << endl;
+    //    for(unsigned j=0; j<fTracklets[i].size(); j+=2){
+    //        fTracklets[i][j].print();
+    //        fTracklets[i][j+1].print();
+    //        cout << endl;
+    //    }
+    //}
 }
- 
-std::vector<double> Reconstruction::GetTrackletParameters(int j)
-{
-    std::vector<double> trackletparameters;
-    double a,b,c,theta,phi; // parameters for line connecting two intersections of the same tracklet from detector 2 to detector 1
-    a = fTracklets[j][0].x - fTracklets[j][1].x;
-    b = fTracklets[j][0].y - fTracklets[j][1].y;
-    c = fTracklets[j][0].z - fTracklets[j][1].z;
-    double norm = TMath::Sqrt(a*a + b*b + c*c);
-    double aa, bb, cc;
-    aa=a/norm;
-    bb=b/norm;
-    cc=c/norm;
-
-    theta=TMath::ACos(cc);
-    phi=TMath::ATan(bb/aa);
-    if (aa<0)
-        phi+=TMath::Pi();
-
-    trackletparameters.push_back(a);
-    trackletparameters.push_back(b);
-    trackletparameters.push_back(c);
-    trackletparameters.push_back(theta);
-    trackletparameters.push_back(phi);
-    return trackletparameters;
-}
-
-/*void Reconstruction::VertexReco(int index, double t)
-{
-    cout << "Entering VertexReco" << endl;
-    std::vector<double> vertex;
-    double a,b,c; // parameters for line connecting two intersections of the same tracklet from detector 2 to detector 1
-    a = fTracklets[index][0].x - fTracklets[index][1].x;
-    b = fTracklets[index][0].y - fTracklets[index][1].y;
-    c = fTracklets[index][0].z - fTracklets[index][1].z;
-
-    // value of parameter t for intersection between line and orthogonal plane passfTracklets[index]ng for O
-    t = -(a*fTracklets[index][1].x + b*fTracklets[index][1].y + c*fTracklets[index][1].z)/(a*a + b*b + c*c);
-
-    // vertex coordfTracklets[index]nates
-    double x, y, z;
-    x = fTracklets[index][1].x + a*t;
-    y = fTracklets[index][1].y + b*t;
-    z = fTracklets[index][1].z + c*t;
-
-    vertex.push_back(x);
-    vertex.push_back(y);
-    vertex.push_back(z);
-
-    fVertexes.push_back(vertex);
-    vertex.clear();
-
-    cout << "Vertex coordinates: ";
-    for(auto i: vertex){
-            cout << i << " ";
-        }
-    cout << endl;
-}*/
 
 void Reconstruction::MinDca()
 {
@@ -183,7 +144,7 @@ void Reconstruction::FillHistoMinDca(TH1D* histo, vector<MaterialBudget::fPoint>
         c = tracklets[j+1].z - tracklets[j].z;
 
         double t;
-        t = - (a*tracklets[j].x + b*tracklets[j].y) / (a*a+b*b);    //t of closest approach
+        t = - (a*tracklets[j].x + b*tracklets[j].y) / (a*a+b*b);    // t of closest approach
 
         vertextemp.push_back(tracklets[j].z + c*t);
         histo->Fill(vertextemp.back());
@@ -196,66 +157,143 @@ void Reconstruction::FillHistoResiduals()
         fResiduals->Fill(fVertexesZ[i]-fConfigs[i][0].z);
 }
 
-vector<double> Reconstruction::Line(int index, double time)
+std::vector<double> Reconstruction::GetTrackletParameters(int nevent, int ntracklet)
 {
-    double x = fTracklets[index][1].x + GetTrackletParameters(index)[0]*time;
-    double y = fTracklets[index][1].y + GetTrackletParameters(index)[1]*time;
-    double z = fTracklets[index][1].z + GetTrackletParameters(index)[2]*time;
+    // returns parameters of tracklet n°ntracklet belonging to the event n°nevent
+    std::vector<double> trackletparameters;
+    double a,b,c,theta,phi; // parameters for line connecting two intersections of the same tracklet from detector 2 to detector 1
+    a = fTracklets[nevent][ntracklet].x - fTracklets[nevent][ntracklet+1].x;
+    b = fTracklets[nevent][ntracklet].y - fTracklets[nevent][ntracklet+1].y;
+    c = fTracklets[nevent][ntracklet].z - fTracklets[nevent][ntracklet+1].z;
+    theta=TMath::ACos(c);
+    phi=TMath::ATan(b/a);
+    if (a<0)
+        phi+=TMath::Pi();
+
+    trackletparameters.push_back(a);
+    trackletparameters.push_back(b);
+    trackletparameters.push_back(c);
+    trackletparameters.push_back(theta);
+    trackletparameters.push_back(phi);
+    return trackletparameters;
+}
+
+vector<double> Reconstruction::Line(int nevent, int index, double time)
+{
+    // returns point of line starting from intersection with detector 2 at t=time of tracklet n°index belonging to the event n°nevent  
+    double x, y, z;
+    x = fTracklets[nevent][index+1].x + GetTrackletParameters(nevent, index)[0]*time;
+    y = fTracklets[nevent][index+1].y + GetTrackletParameters(nevent, index)[1]*time;
+    z = fTracklets[nevent][index+1].z + GetTrackletParameters(nevent, index)[2]*time;
+    //cout << x << " " << y << " " << z << endl;
     vector<double> linecoordinates;
     linecoordinates.push_back(x);
     linecoordinates.push_back(y);
     linecoordinates.push_back(z);
+    //cout << "Line Ending" << endl;
     return linecoordinates;
 }
 
 void Reconstruction::MinGlobalDistance()
 {
-    int index=0;
-    int phi=abs(fTracklets[0][0].phi);
-    for(unsigned i=1; i<fTracklets.size(); ++i){
-        if(abs(fTracklets[i][0].phi)<phi){
-            phi=fTracklets[i][0].phi;
-            index=i;
+    for(int nevent=0; nevent<fTracklets.size(); nevent++){
+        cout << "Entering MinGlobalDistance" << endl;
+            
+            // First step: find tracklet with smallest phi
+        int index=0;
+        int phi=abs(fTracklets[nevent][0].phi);
+        for(unsigned i=2; i<fTracklets[nevent].size(); i+=2){
+            if(abs(fTracklets[nevent][i].phi)<phi){
+                phi=fTracklets[nevent][i].phi;
+                index=i;
+            }
         }
-    }
-    vector<double> distances;
-    MaterialBudget::fPoint point0 = FirstOctant(fTracklets[index][1]);
-    for(unsigned i=0; i<fTracklets.size(); ++i){
-        // if(i==index) distances.push_back(0.);
-        MaterialBudget::fPoint point1 = FirstOctant(fTracklets[i][1]);
-        double DX = point1.x-point0.x; 
-        double DY = point1.y-point0.y;
-        double DZ = point1.z-point0.z;
-        distances.push_back(TMath::Sqrt(DX*DX + DY*DY + DZ*DZ));
-    }
-    vector<double> times;
-    for(auto i: distances){
-        times.push_back(i/(3.*10*10*10*10*10*10*10*10));
-    }
-    double totaldistance = 0.;
-    double t;
-    for(unsigned y=0; y<fTracklets.size(); y++){
-        for(unsigned j=0; j<fTracklets.size(); j++){
-            double dx = Line(y,t+times[y])[0]-Line(j,t+times[j])[0];
-            double dy = Line(y,t+times[y])[1]-Line(j,t+times[j])[1];
-            double dz = Line(y,t+times[y])[2]-Line(j,t+times[j])[2];
-            totaldistance += TMath::Sqrt(dx*dx + dy*dy + dz*dz);
+            // Second step: calculate norm of difference vector between equivalent points belonging to the octant x,y,z>0 of intersections
+            // with 2nd detector of all tracklets and tracklet with minimum phi
+        std::vector<double> distances;
+        MaterialBudget::fPoint point0 = FirstOctant(fTracklets[nevent][index+1]); // intersection of tracklet with minimum phi in first octant
+        for(unsigned i=0; i<fTracklets[nevent].size(); i+=2){
+            MaterialBudget::fPoint point1 = FirstOctant(fTracklets[nevent][i+1]);
+            double DX = point1.x-point0.x; 
+            double DY = point1.y-point0.y;
+            double DZ = point1.z-point0.z;
+            distances.push_back(TMath::Sqrt(DX*DX + DY*DY + DZ*DZ));
         }
+            // Third step: connecting spacial delays with time delays
+        std::vector<double> delays;
+        for(auto i: distances){
+            delays.push_back(i);
+            delays.push_back(i);
+        }
+
+        // Fourth step: declare histogram with time on x axis, total distance of all tracklet points on y axis
+        int nbins = 1000;
+        TH1D* hist = new TH1D("TrackDcaVertex", "TrackDcaVertex", nbins, 0, 10);
+        
+        // Fifth step: fill histogram
+        for(unsigned int i=1; i<=nbins; i++){
+            hist->Fill(i,FillHistoTrackMinDca(nevent, hist->GetBinCenter(i), delays));
+        }
+
+        int binmin = hist->GetMinimumBin();
+        double t0 = hist->GetXaxis()->GetBinCenter(binmin);
+
+        for(unsigned i=0; i<fTracklets[nevent].size(); i+=2){
+            std::vector<double> trackletvertex = Line(nevent, i, t0+delays[i]);
+            fVertexesTrackDca.push_back(trackletvertex);
+            fVertexesZTrackDca.push_back(trackletvertex[2]);
+        }
+
+        cout << "Reconstructed vertexes: " << endl;
+        for(unsigned b=0; b<fVertexesZTrackDca.size(); b++){
+            cout << fVertexesZTrackDca[b] << "    ";
+        }
+
+        delete hist;
     }
 }
 
-vector<double> Reconstruction::TimesMinGlobalDistance(int index0)
+double Reconstruction::FillHistoTrackMinDca(int nevent, double time, vector<double> delays)
 {
-    double a,b,c; // parameters for line connecting two intersections of the same tracklet from detector 2 to detector 1
-    a = fTracklets[index0][0].x - fTracklets[index0][1].x;
-    b = fTracklets[index0][0].y - fTracklets[index0][1].y;
-    c = fTracklets[index0][0].z - fTracklets[index0][1].z;
-    double t;
-    t = - (a*fTracklets[index0][1].x + b*fTracklets[index0][1].y) / (a*a+b*b);    //t of closest approach
+    // fills histogram with total distance between tracklets at time t
+    cout << "Entering FillHistoTrackMinDca" << endl;
+    
+        // Sixth step: loop over all tracklets to find total distance of all tracklets with respect to all tracklets
+    double totaldistance=0; // total distance
+    
+    for(unsigned w=0; w<fTracklets[nevent].size(); w+=2){
+        // sum total distance of all tracklets with respect to the w-th tracklet of the nevent-th event
+        totaldistance += GetTotalDistance(nevent, w, time+delays[w]);
+    }
+    cout << "Filling histogram entry for time: " << time << endl;
+    return totaldistance;
 }
 
-MaterialBudget::fPoint Reconstruction::FirstOctant(MaterialBudget::fPoint point) // turns point into a vector in x,y,z>0 octant with minimum phi
+double Reconstruction::GetTotalDistance(int nevent, unsigned trackindex, double time)
+{    
+        //returns total distance of all tracklets with respect to track0 at t=time
+    //cout << "Entering GetTotalDistance for track number " << trackindex << endl;
+    std::vector<double> vec1 = Line(nevent,trackindex,time);
+    
+    double countdistance = 0.;
+    for(unsigned z=0; z<fTracklets[nevent].size(); z+=2){
+        // loop over all tracklets to sum distance between track0 point and their line point at time t 
+        std::vector<double> vec2 = Line(nevent,z,time);
+        double dx = vec1[0]-vec2[0];
+        double dy = vec1[1]-vec2[1];
+        double dz = vec1[2]-vec2[2];
+        //cout << dx << " " << dy << " " << dz << endl;
+        countdistance += TMath::Sqrt(dx*dx + dy*dy + dz*dz);
+    }
+    return countdistance;
+
+}
+
+MaterialBudget::fPoint Reconstruction::FirstOctant(MaterialBudget::fPoint point)
 {
+        // turns generic point into a point in x,y,z>0 octant on the cone defined by the same theta
+    cout << "Entering FirstOctant" << endl;
+    
     double x = point.x; 
     double y = point.y; 
     double z = point.z;
@@ -270,3 +308,35 @@ MaterialBudget::fPoint Reconstruction::FirstOctant(MaterialBudget::fPoint point)
 
     return point;
 }
+
+/*void Reconstruction::VertexReco(int index, double t)
+{
+    cout << "Entering VertexReco" << endl;
+    std::vector<double> vertex;
+    double a,b,c; // parameters for line connecting two intersections of the same tracklet from detector 2 to detector 1
+    a = fTracklets[index][0].x - fTracklets[index][1].x;
+    b = fTracklets[index][0].y - fTracklets[index][1].y;
+    c = fTracklets[index][0].z - fTracklets[index][1].z;
+
+    // value of parameter t for intersection between line and orthogonal plane passfTracklets[index]ng for O
+    t = -(a*fTracklets[index][1].x + b*fTracklets[index][1].y + c*fTracklets[index][1].z)/(a*a + b*b + c*c);
+
+    // vertex coordfTracklets[index]nates
+    double x, y, z;
+    x = fTracklets[index][1].x + a*t;
+    y = fTracklets[index][1].y + b*t;
+    z = fTracklets[index][1].z + c*t;
+
+    vertex.push_back(x);
+    vertex.push_back(y);
+    vertex.push_back(z);
+
+    fVertexes.push_back(vertex);
+    vertex.clear();
+
+    cout << "Vertex coordinates: ";
+    for(auto i: vertex){
+            cout << i << " ";
+        }
+    cout << endl;
+}*/
--- Simulation/Reconstruction.h
+++ Simulation/Reconstruction.h
@@ -17,12 +17,14 @@ public:
     void VertexReco();
     void MinDca();
     void TrackletsReco();
-    std::vector<double> GetTrackletParameters(int j);
+    std::vector<double> GetTrackletParameters(int nevent, int ntracklet);
     // void VertexReco(int index, double t);
     void MinGlobalDistance();
     MaterialBudget::fPoint FirstOctant(MaterialBudget::fPoint point);
-    vector<double> Line(int index, double time);
-    vector<double> TimesMinGlobalDistance(int index0);
+    vector<double> Line(int nevent, int index, double time);
+    double FillHistoTrackMinDca(int nevent, double time, vector<double> delays);
+    double GetTotalDistance(int nevent, unsigned trackindex, double time);
+    // vector<double> TimesMinGlobalDistance(int index0);
  
 private:
     std::vector<std::vector<MaterialBudget::fPoint>> fIntersections1;
@@ -31,6 +33,8 @@ private:
     std::vector<std::vector<MaterialBudget::fPoint>> fTracklets;
     std::vector<std::vector<double>> fVertexes;
     std::vector<double> fVertexesZ;
+    std::vector<std::vector<double>> fVertexesTrackDca;
+    std::vector<double> fVertexesZTrackDca;
     TH1D* fResiduals = new TH1D("Residuals", "Residuals", 500,-0.5,0.5);
 
     void FillHistoMinDca(TH1D* histo, vector<MaterialBudget::fPoint>& tracklets, vector<double>& vertextemp);
